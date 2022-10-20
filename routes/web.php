caf3b7dd920b46f1aeecce547c7efa0309e9adfe
dwahyu87 <wahyudewangga395@gmail.com>
2022-10-20 15:46:27 +0700

    tugass

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use App\Http\Controllers\LatihanController;
 use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller as RoutingController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 
 
 Route::get('/home', function () {
@@ -11,7 +16,9 @@ Route::get('/home', function () {
 
 Route::get('/latihan', [LatihanController::class, 'home']);
 Route::get('/beranda', [LatihanController::class, 'beranda']);
-Route::get('/homepage', [HomepageController::class, 'index']);
+Route::get('/index', [HomepageController::class, 'index']);
 Route::get('/about', [HomepageController::class, 'about']);
 Route::get('/kontak', [HomepageController::class, 'kontak']);
 Route::get('/kategori', [HomepageController::class, 'kategori']);
+Route::get('/admin', [DashboardController::class, 'index']);
+

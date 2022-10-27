6c60ae1668ee276fab2d640342c9f3e4620e7d04
dwahyu87 <wahyudewangga395@gmail.com>
2022-10-27 13:45:46 +0700

    Jawaban Soal 1

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -6,8 +6,5 @@ use Illuminate\Http\Request;
 
 class KategoriController extends Controller
 {
-    public function index (){
-        $data = array('title' => 'Kategori');
-        return view('Kategori.index', $data);
-    }
+    //
 }
--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,30 @@ use Illuminate\Support\Facades\Auth;
 
 
 
-Route::get('/home', function () {
-    return "halo";
+
+// Route::get('/', [HomepageController::class, 'index']);
+Route::prefix('mahasiswa')->group(function () {
+
+    Route::get('pendaftaran', function () {
+        echo "<h2>Halaman Pendaftaran</h2>";
+    });
+
+    Route::get('ujian', function () {
+        echo "<h2>Halaman Ujian</h2>";
+    });
+
+    Route::get('nilai', function () {
+        echo "<h2>Halaman Nilai</h2>";
+    });
+
 });
 
-Route::get('/latihan', [LatihanController::class, 'home']);
-Route::get('/beranda', [LatihanController::class, 'beranda']);
-Route::get('/index', [HomepageController::class, 'index']);
-Route::get('/about', [HomepageController::class, 'about']);
-Route::get('/kontak', [HomepageController::class, 'kontak']);
-Route::get('/kategori', [HomepageController::class, 'kategori']);
-Route::get('/admin', [DashboardController::class, 'index']);
+// Route::get('/latihan', [LatihanController::class, 'home']);
+// Route::get('/beranda', [LatihanController::class, 'beranda']);
+// Route::get('/index', [HomepageController::class, 'index']);
+// Route::get('/about', [HomepageController::class, 'about']);
+// Route::get('/kontak', [HomepageController::class, 'kontak']);
+// Route::get('/kategori', [HomepageController::class, 'kategori']);
+// Route::get('/admin', [DashboardController::class, 'index']);
+
 
